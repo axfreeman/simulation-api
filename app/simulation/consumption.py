7a44bc6256308b146099b7bed645abb84e6537b1
Alan Freeman <afreeman@iwgvt.org>
2024-06-11 15:08:58 +0100

    Fixing Consumption -does not set sales stock of classes correctly

--- app/simulation/consumption.py
+++ app/simulation/consumption.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from app.models import SocialClass, Simulation, Class_stock
+from app.models import Commodity, SocialClass, Simulation, Class_stock
 from ..logging import report
 
 """This module contains functions needed to implement the consumption action.
@@ -18,40 +18,44 @@ def consume(session:Session, simulation:Simulation)->str:
 
     return "Consumption complete"
 
-def class_consume(social_class:SocialClass, session:Session, simulation:Simulation):
+def class_consume(
+        social_class:SocialClass, 
+        session:Session, 
+        simulation:Simulation):
     """Tell one social class to consume and reproduce its product if it has one.
     No population dynamics at present - just consumption.
+
+        social_class(SocialClass):
+            the class which is being processed
+
+        session(Session):
+            the sqlAlchemy session which will store the data
+
+        simulation(Simulation):
+            the Simulation currently under way
     """
-    report(2,simulation.id,
-        f"Social Class {social_class.name} is reproducing itself",session,
-    )
+    report(2,simulation.id,f"Social Class {social_class.name} is reproducing",session)
 
-    ss = social_class.sales_stock(session)
-    session.add(ss)
+    sales_stock = social_class.sales_stock(session)
+    sales_commodity:Commodity=sales_stock.commodity(session)
+    session.add(sales_stock)
 
-    report(2,simulation.id,
-        f"Sales stock size before consumption is {ss.size} with value {ss.value}",session,
-    )
+    report(2,simulation.id,f"Sales stock size before consumption is {sales_stock.size} with value {sales_stock.value}",session)
 
-    consuming_stocks_query = session.query(Class_stock).where(
+    consumption_stocks_query = session.query(Class_stock).where(
         Class_stock.simulation_id == simulation.id,
         Class_stock.class_id == social_class.id,
         Class_stock.usage_type == "Consumption",
     )
 
-    for stock in consuming_stocks_query:
+    for stock in consumption_stocks_query:
         session.add(stock)
         commodity=stock.commodity(session)
-        report(3,simulation.id,
-            f"Consuming stock of {stock.name} of usage type {stock.usage_type} whose size is {stock.size}",session,
-        )
-        
+        report(3,simulation.id,f"Consuming stock of {stock.name} with size  {stock.size} and value {stock.value}",session)
         stock.size -=stock.flow_per_period(session)  # eat according to defined consumption standards
         stock.price-=stock.flow_per_period(session)*commodity.unit_price
         stock.value-=stock.flow_per_period(session)*commodity.unit_value
-    report(3,simulation.id,
-        f"Replenishing the sales stock of {social_class.name} with a population of {social_class.population}",session,
-    )
+        report(3,simulation.id,f"Consumption stock {stock.name} now has size {stock.size} and value {stock.value}",session)
     
     # Currently no population dynamics and no differential labour intensity
     # Capitalists are assumed here (as per Cheng et al.) to supply services
@@ -59,11 +63,11 @@ def class_consume(social_class:SocialClass, session:Session, simulation:Simulati
     # have to supply in proportion to their capital. Others who believe this
     # nonsense will have to construct algorithms instantiating it if they
     # wish to test it logically.
-    ss.size = (
-        social_class.population
-    )
-
-    report(3,simulation.id,
-        f"Supply of {ss.name} has reached {ss.size}",session,
-    )
+    #
+    # TODO We also do not calculate the unit value of factors, but we should
+    report(3,simulation.id,f"Replenishing the sales stock {sales_stock.name} of {social_class.name} whose population is {social_class.population}",session)
+    report(3,simulation.id,f"Its size before replenishment is {sales_stock.size} with value {sales_stock.value}",session)
+    sales_stock.size += (social_class.population/simulation.periods_per_year)
+    sales_stock.value += (social_class.population/simulation.periods_per_year)*sales_commodity.unit_value
+    report(3,simulation.id,f"Its size is now {sales_stock.size} with value {sales_stock.value}",session)
     session.commit()
